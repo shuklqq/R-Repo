51f08790bedfb7307c824f6b8909e4b6eb3e2e13
shuklqq <160501897+shuklqq@users.noreply.github.com>
2024-02-24 21:59:50 +0530

    Added new class for SubArrayWithZeroSumUsingHashMap

---
+++ Practice/src/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,13 @@
+public class GreatestCommonDivisorOfStrings {
+    public String gcdOfStrings(String str1, String st2){
+        int a = str1.length();
+        int b = st2.length();
+        String result = "";
+
+        if(a < b){
+            
+        }
+
+    return result;
+    }
+}
--- Practice/src/Main.java
+++ Practice/src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//Greatest Common DivisorOfStrings
+//    /*
+//    * Variable Declarations
+//    * */
+//        String str1 = "";
+//        String str2 = "";
+//        String result = "";
+//
+//    /*
+//    * Method calls
+//    *
+//    * */
+//        GreatestCommonDivisorOfStrings a  = new GreatestCommonDivisorOfStrings();
+//        result = a.gcdOfStrings(str1,str2);
+//
+//    /*
+//    * Response Print
+//    *
+//    * */
+//
+//        System.out.println("Result of the Method = "+ result);
+
+
+//SubArrayWithZeroSum
+    SubArrayWithZeroSumUsingHashMap a = new SubArrayWithZeroSumUsingHashMap();
+    a.InvokeStart();
+
+
+
+
     }
 }
