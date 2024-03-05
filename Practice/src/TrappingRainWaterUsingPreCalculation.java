1fcc8e92bb65d958f3f54d28525aafafc29d9710
shuklqq <160501897+shuklqq@users.noreply.github.com>
2024-03-05 12:29:04 +0530

    added new array quuestions.

---
+++ Practice/src/TrappingRainWaterUsingPreCalculation.java
@@ -0,0 +1,11 @@
+public class TrappingRainWater {
+    static int Trap(int arr[], int n){
+
+    }
+    static void InvokeStart(){
+        int[] arr = {};
+        int len = arr.length;
+
+        System.out.println();
+    }
+}
---
+++ Practice/src/TrappingRainWaterUsingStack.java
@@ -0,0 +1,11 @@
+public class TrappingRainWaterUusingStack {
+    public long Trap(int arr[], int n){
+
+    }
+    public void InvokeStart(){
+        int[] a = {3,0,0,2,0,4};
+        int len = a.length;
+
+        System.out.println("The total amount of rain water stored is : " + Trap(a,len));
+    }
+}
